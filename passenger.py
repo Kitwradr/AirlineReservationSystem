2aace529f4a60df0e79c44c065d4cc68d8b903a4
Suhas Poornachandra <suhaspoornachandra@gmail.com>
2018-11-13 22:05:20 +0530

    add database

---
+++ passenger.py
@@ -0,0 +1,51 @@
+import sqlite3
+import json
+from Transaction import *
+from Order import *
+import hashlib, os
+
+class Customer(object):
+    def __init__(self,email=None,password=None,id_=None):
+        if id_ is None and email is not None and password is not None:
+            self.status = self._login(email,password)
+        else:
+            self._email = email
+            self.status = True
+
+        self._email = ""
+        if email is not None:
+            self._email = email
+
+        self._phoneno = 0
+        
+        self._name = ""
+        self._address = ""
+        self.db = sqlite3.connect('database.db')
+
+
+        
+    def _login(self,email, password):
+        with sqlite3.connect('database.db') as con
+        try:
+        	cur=con.cursor()
+        	cur.execute('SELECT password from PASSENGER WHERE email=?',(email,))
+        	data=cur.fetchall()
+        	if(data==password)
+        	return True
+        	else
+        	return False
+
+
+        def register(password, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone):
+        with sqlite3.connect('database.db') as con:
+            try:
+                cur = con.cursor()
+                cur.execute('INSERT INTO users (password, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', password, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone)
+                con.commit()
+
+                msg = "Registered Successfully"
+            except:
+                con.rollback()
+                msg = "Error occured"
+        con.close()
+        return msg
